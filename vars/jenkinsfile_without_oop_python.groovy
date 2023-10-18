14c2aa8f71951b662ee9068f5b5f1eb09af69801
sarsatis <sarthak8055@gmail.com>
2023-10-18 19:27:59 +0100

    sa

--- vars/jenkinsfile_without_oop_python.groovy
+++ vars/jenkinsfile_without_oop_python.groovy
@@ -8,7 +8,7 @@ def call(){
         agent {
             kubernetes {
                 inheritFrom 'jenkins-${UUID.randomUUID().toString()}'
-                yamlFile "$podTemplate"
+                yamlFile = readFile('resources/pod-template.yaml')
             }
         }
         environment {
