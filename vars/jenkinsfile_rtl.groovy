5dfb861262de2c372ab4f5f9ea65c2575e5a5ad4
sarsatis <sarthak8055@gmail.com>
2023-10-30 18:00:32 +0000

    sa

---
+++ vars/jenkinsfile_rtl.groovy
@@ -0,0 +1,30 @@
+def call(){
+    def podTemplate = "podTemplate.yaml"
+    def prScript = libraryResource "pr.sh"
+
+    pipeline {
+        agent {
+            kubernetes {
+                label "jenkins-${UUID.randomUUID().toString()}"
+                yamlFile "$podTemplate"
+            }
+        }
+        environment {
+            NAME = "${jobNameParts[0]}"
+            VERSION = "${env.GIT_COMMIT}-${env.BUILD_ID}"
+            IMAGE_REPO = "sarthaksatish"
+            GITHUB_TOKEN = credentials('githubpat')
+            NAMESPACE = "jenkins"
+            HELM_CHART_DIRECTORY = "charts/priyankalearnings"
+        }
+
+        stages {
+            stage('Unit Tests') {
+                steps {
+                    echo 'Implement unit tests if applicable.'
+                    echo 'This stage is a sample placeholder'
+                }
+            }
+        }
+    }
+}
